5ed0f43ab1f539e1902ed999590262d792a5507a
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-15 11:41:38 +0100

    feat: bonus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
       submitButton.addEventListener("click", function (event){
-         event.preventDefault
+         event.preventDefault()
 
       let distance = document.querySelector("#distance")
       let age = document.querySelector("#age")
@@ -7,7 +7,21 @@
       let price = document.querySelector("#price")
       let ticket = 0.21 * Number(distance.value)
       let finalPrice = price.innerHTML = `Il prezzo del biglietto è: ${ticket.toFixed(2).replace(`.`, `,`)}€`
+      //loading
+      const spinnerEl = document.querySelector("#spinner")
+      const statusSpan = document.querySelector(".status")
+      const orginalText = statusSpan.innerHTML
 
+      submitButton.disabled = true
+      spinnerEl.classList.toggle(`d-none`)
+      statusSpan.innerHTML = `Loading....`
+      
+      let loading = setTimeout(function(){
+      submitButton.disabled = false
+      spinnerEl.classList.toggle(`d-none`)
+      statusSpan.innerHTML = orginalText
+      }, 3000);
+      
          if(age.value >= 65){
             ticket -= (ticket*0.4)
             finalPrice
