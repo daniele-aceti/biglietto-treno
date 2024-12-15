7aea8571d79ae9206a4c608ce8ef6c6435d44dc5
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-15 13:46:51 +0100

    feat: price bonus

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@
       let age = document.querySelector("#age")
       let submitButton = document.querySelector("#submitButton")
       let price = document.querySelector("#price")
-      let ticket = 0.21 * Number(distance.value)
-      let finalPrice = price.innerHTML = `Il prezzo del biglietto è: ${ticket.toFixed(2).replace(`.`, `,`)}€`
+      
       //loading
       const spinnerEl = document.querySelector("#spinner")
       const statusSpan = document.querySelector(".status")
@@ -17,20 +16,26 @@
       statusSpan.innerHTML = `Loading....`
       
       let loading = setTimeout(function(){
+         //load button
       submitButton.disabled = false
       spinnerEl.classList.toggle(`d-none`)
       statusSpan.innerHTML = orginalText
-      }, 3000);
-      
-         if(age.value >= 65){
+
+      //load price
+
+      let ticket = 0.21 * Number(distance.value)
+      let finalPrice = price.innerHTML = `Il prezzo del biglietto è: ${ticket.toFixed(2).replace(`.`, `,`)}€`
+
+      if(age.value >= 65){
             ticket -= (ticket*0.4)
             finalPrice
-         }else if (age.value <= 18){
+      }else if (age.value <= 18){
              ticket -= (ticket*0.2)
              finalPrice
-         }else{
+      }else{
             finalPrice
-         }
+      }
+      }, 3000);   
          })
          
          
