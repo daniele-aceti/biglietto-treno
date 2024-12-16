a008b0c812a2d9e573de4ed98f8dc877228bfc11
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-16 10:14:26 +0100

    feat: loading text

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
       submitButton.addEventListener("click", function (event){
-         event.preventDefault()
+      event.preventDefault()
 
       let distance = document.querySelector("#distance")
       let age = document.querySelector("#age")
       let submitButton = document.querySelector("#submitButton")
       let price = document.querySelector("#price")
       
-      //loading
+       //load text
+
+      const spinnerText = document.querySelector(".spinnerT")
+      spinnerText.classList.toggle("d-none")
+      price.classList.toggle("d-none")
+      
+      //load Button
       const spinnerEl = document.querySelector("#spinner")
       const statusSpan = document.querySelector(".status")
       const orginalText = statusSpan.innerHTML
 
       submitButton.disabled = true
-      spinnerEl.classList.toggle(`d-none`)
+      spinnerEl.classList.toggle("d-none")
       statusSpan.innerHTML = `Loading....`
+
       
+      // three seconds
+
       let loading = setTimeout(function(){
-         //load button
+
+      //load text
+      spinnerText.classList.toggle("d-none")
+      price.classList.toggle("d-none")
+
+         
+      //load button
       submitButton.disabled = false
       spinnerEl.classList.toggle(`d-none`)
       statusSpan.innerHTML = orginalText
@@ -25,6 +40,8 @@
 
       let ticket = 0.21 * Number(distance.value)
       let finalPrice = price.innerHTML = `Il prezzo del biglietto è: ${ticket.toFixed(2).replace(`.`, `,`)}€`
+
+      console.log(distance + age)
 
       if(age.value >= 65){
             ticket -= (ticket*0.4)
